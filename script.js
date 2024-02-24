791c03880ef66515860358b32bdd8c32e096e19d
Lune3 <113820374+Lune3@users.noreply.github.com>
2024-02-24 19:13:11 +0530

    Added error checking

--- script.js
+++ script.js
@@ -6,7 +6,15 @@ const gameBoard = (function (){
     function appendCell(cell,character){
         grid[cell] = character;
     }
-    return {appendCell};
+    function isAlreadyfilled(cell){
+        if(grid[cell] != -1){
+            return true;
+        }
+        else{
+        return false;
+        }
+    }
+    return {appendCell,isAlreadyfilled};
 })();
 
 const players = (function (){
@@ -21,10 +29,16 @@ const players = (function (){
 
 const gameFlow = function (){
     let playerTurn = 1;
-    
+
     function gameStart(){
         for(let i = 0;i < 9;i++){
-            const cell = parseInt(prompt("Select your cell"));
+
+            let cell = parseInt(prompt("Select your cell")) - 1;
+
+            while(checkForErrors(cell)){
+                cell = parseInt(prompt("Select a valid cell")) - 1;
+            }
+
             if(playerTurn === 1){
                 players.cellSelected(cell,playerTurn);
                 gameBoard.appendCell(cell,"X");
@@ -33,6 +47,20 @@ const gameFlow = function (){
                 players.cellSelected(cell,playerTurn);
                 gameBoard.appendCell(cell,"O");
             }
+
+            playerTurn = 0;
         }
     }
+    function checkForErrors(cell){
+        if(typeof(cell) != "number" ||cell < 0 || cell > 8 ||gameBoard.isAlreadyfilled(cell)){
+            return true;
+        }
+        else{
+        return false;
+        }
+    }
+    return {gameStart};
 }
+
+const start = gameFlow();
+start.gameStart();
