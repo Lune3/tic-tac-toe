d2c13670b7dc57457c78817fd9a35b7c153172a0
Lune3 <113820374+Lune3@users.noreply.github.com>
2024-03-21 20:15:22 +0530

    Added clear to clear board after game ends

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 const gameBoard = (function (){
-    const grid = [];
-    for(let i = 0;i < 9;i++){
-        grid.push(-1);
+    let grid = [];
+    function initializeBoard(){
+        grid = [];
+        for(let i = 0;i < 9;i++){
+            grid.push(-1);
+        }
     }
 
     function appendCell(cell,character){
         grid[cell] = character;
-        console.log(grid);
     }
 
     function isAlreadyfilled(cell){
@@ -17,32 +19,29 @@ const gameBoard = (function (){
         return false;
         }
     }
-    return {appendCell,isAlreadyfilled};
+    return {appendCell,isAlreadyfilled,initializeBoard};
 })();
 
 const players = (function (){
-    const player1 = [];
-    const player2 = [];
+    let player1 = [];
+    let player2 = [];
+
+    function clearPlayerTurns(){
+        player1 = [];
+        player2 = [];
+    }
 
     function cellSelected(cell,playerTurn){
-        if(!playerTurn){
+        if(playerTurn){
             player1.push(cell);
             player1.sort(function(a,b){return a - b});
-            console.log("player 1 = ",player1);
         } 
         else{
             player2.push(cell);
             player2.sort(function(a,b){return a - b});
-            console.log("player 2 = ",player2);
         }
     }
 
-    function arrayEquals(a, b) {
-        return Array.isArray(a) &&
-            Array.isArray(b) &&
-            a.length === b.length &&
-            a.every((val, index) => val === b[index]);
-    }
 
     function checkForWinP1(){ 
         const winningPattern = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
@@ -67,23 +66,22 @@ const players = (function (){
         }
         return false;
     }
-    return {cellSelected,checkForWinP1,checkForWinP2};
+    return {cellSelected,checkForWinP1,checkForWinP2,clearPlayerTurns};
 })();
 
 const gameFlow = function (){
-    let playerTurn = false;
-
     const grid = document.querySelectorAll("li");
-    
-
+    let playerTurn = true;
+    let counter = 0;
     function gameStart(){
+        gameBoard.initializeBoard();
         grid.forEach(containers => {
             containers.addEventListener("click" , (e) =>{
                 let cell = e.target.className;
                 cell = parseInt(cell.charAt(cell.length - 1)) - 1;
                 if(!gameBoard.isAlreadyfilled(cell)){
                     players.cellSelected(cell,playerTurn); 
-                    if(playerTurn){
+                    if(!playerTurn){
                         gameBoard.appendCell(cell,"O");
                         const circle = document.createElement("img");
                         circle.src = "Images/1811_circle.png";
@@ -96,14 +94,19 @@ const gameFlow = function (){
                         e.target.appendChild(cross);
                     }
                     playerTurn = !playerTurn;
+                    counter++;
                 }
                 if(players.checkForWinP1()){
                     endGame(1);
                     return;
                 }
-                if(players.checkForWinP2()){
+                else if(players.checkForWinP2()){
                     endGame(2);
                     return;
+                }
+                else if(counter === 9){
+                    endGame(3);
+                    return ;
                 }
             })
         });
@@ -113,22 +116,36 @@ const gameFlow = function (){
     function endGame(winningArr){
         const dialog = document.querySelector("dialog");
         const button = document.querySelector("button");
+        const dialogWindow = document.querySelector("p");
         if(winningArr === 1){
             dialog.showModal();
-            const dialogWindow = document.querySelector("p");
             dialogWindow.textContent = "Player 1 has won the game";
         }
         else if(winningArr === 2){
-            alert("Player 2 has won the game");
+            dialog.showModal();
+            dialogWindow.textContent = "Player 2 has won the game";
         }
-        else{
-            alert("It's a tie");
+        else if(winningArr == 3){
+            dialog.showModal();
+            dialogWindow.textContent = "It's a tie";
         }
         button.addEventListener("click",() =>{
             dialog.close();
+            clear();
         })
     }
-    return {gameStart,endGame};
+
+    function clear(){
+        players.clearPlayerTurns();
+        grid.forEach(gridContainer => {
+            gridContainer.textContent = "";
+        });
+        gameStart();
+        playerTurn = true;
+        counter = 0;
+    }
+
+    return {gameStart};
 }
 
 const start = gameFlow();
